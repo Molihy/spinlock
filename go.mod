50dbcf6ff1013b0c0e87f092f512ec03ed3374f7
molikatty <nicemolixi@gmail.com>
2023-02-08 13:12:32 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/molikatty/mlock
+
+go 1.20
---
+++ lock.go
@@ -0,0 +1,33 @@
+// https://github.com/panjf2000/ants/blob/88d2454bbb85fc005ae0eea4f33d3cf644554aa9/internal/spinlock.go#L17
+package mlock
+
+import (
+	"runtime"
+	"sync"
+	"sync/atomic"
+)
+
+type spinLock uint32
+
+const maxBackoff = 16
+
+func (sl *spinLock) Lock() {
+	backoff := 1
+	for !atomic.CompareAndSwapUint32((*uint32)(sl), 0, 1) {
+		// https://en.wikipedia.org/wiki/Exponential_backoff.
+		for i := 0; i < backoff; i++ {
+			runtime.Gosched()
+		}
+		if backoff < maxBackoff {
+			backoff <<= 1
+		}
+	}
+}
+
+func (sl *spinLock) Unlock() {
+	atomic.StoreUint32((*uint32)(sl), 0)
+}
+
+func NewSpinLock() sync.Locker {
+	return new(spinLock)
+}
